669dc8e6b9a75e11d9d9cba1f7286182323fdc9b
myNameIsJeff <123896394+FreDo-JS@users.noreply.github.com>
2024-12-30 21:12:05 +0100

    foty

---
+++ Hotel/Views/Główna/GłównaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Views.Główna
+{
+    public class GłównaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
